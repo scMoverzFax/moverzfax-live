96cec74468a17c25ce510af603ee1ea60e322027
scMoverzFax <scMoverzFax@gmail.com>
2023-02-22 17:48:06 -0600

    asa

--- admin/admin_model/connection.php
+++ admin/admin_model/connection.php
@@ -1,4 +1,3 @@
-
 <?php
 $servername="focus";
 $username="moverz20_moverz_sc";
@@ -8,16 +7,4 @@ $con=mysqli_connect($servername,$username,$password,$dbname);
 	if (!$con){
 		die ("Connection failed : " .mysqli_connect_error());
 	}
-// $host="localhost";
-// $user="id17415443_root";
-// $password="(9AP_5gbp{|@63<-";
-// $dbname="id17415443_move";
-
-// $servername="localhost";
-// $username="root";
-// $password="";
-// $dbname="move"; 
-
-// $con=mysqli_connect($servername,$username,$password,$dbname);
-
 ?>
