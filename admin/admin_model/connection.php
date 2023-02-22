b038fa5ba812691e1415c2312c9fbac59cfeab81
scMoverzFax <scMoverzFax@gmail.com>
2023-02-22 17:20:53 -0600

    dbcon

--- admin/admin_model/connection.php
+++ admin/admin_model/connection.php
@@ -1,15 +1,23 @@
 
 <?php
+$servername="focus";
+$username="moverz20_moverz_sc";
+$password="WgLgvmjLCQ4jPt6";
+$dbname="moverz20_move";
+$con=mysqli_connect($servername,$username,$password,$dbname);
+	if (!$con){
+		die ("Connection failed : " .mysqli_connect_error());
+	}
 // $host="localhost";
 // $user="id17415443_root";
 // $password="(9AP_5gbp{|@63<-";
 // $dbname="id17415443_move";
 
-$servername="localhost";
-$username="root";
-$password="";
-$dbname="move";
+// $servername="localhost";
+// $username="root";
+// $password="";
+// $dbname="move"; 
 
-$con=mysqli_connect($servername,$username,$password,$dbname);
+// $con=mysqli_connect($servername,$username,$password,$dbname);
 
 ?>
