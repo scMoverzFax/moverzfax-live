b3a231b17225699cfc295df55f190284556445e0
scMoverzFax <scMoverzFax@gmail.com>
2023-02-22 16:44:06 -0600

    where r

--- home/index.php
+++ home/index.php
@@ -713,59 +713,44 @@
                 <div class="col-md-12 text-center">
                     <h1 style="font-weight: 500">Partners and Collaborators</h1>
                 </div>
+
+                <!-- -------- -->
+
                 <div style="display: flex; justify-content: center; align-items: center;">
+                
                     <div class="col-md-12 m-b15">
                         <span class="client-item " style="width: 23%">
-                            <!-- <a href="javascript:;"> -->
                             <img src="../img/moversfax/01.png" class="img-responsive first-item" style="display: block; cursor: default;" alt="">
-                            <!-- <img src="../img/moversfax/01-g.png" class="color-img img-responsive" style="display: none;" alt=""> -->
-                            <!-- </a> -->
                         </span>
                         <span class="client-item" style="width: 13%">
-                            <!-- <a href="javascript:;"> -->
                             <img src="../img/moversfax/02.png" class="img-responsive first-item" style="display: block; cursor: default;" alt="">
-                            <!-- <img src="../img/moversfax/02-g.png" class="color-img img-responsive" style="display: none; border: solid;" alt=""> -->
-                            <!-- </a> -->
                         </span>
                         <span class="client-item" style="width: 27%">
-                            <!-- <a href="javascript:;"> -->
                             <img src="../img/moversfax/03.png" class="img-responsive first-item" style="display: block; cursor: default;" alt="">
-                            <!-- <img src="../img/moversfax/03-g.png" class="color-img img-responsive" style="display: block; border: solid;" alt=""> -->
-                            <!-- </a> -->
                         </span>
                     </div>
 
                     <div class="col-md-12 m-b15">
                         <span class="client-item" style="width: 24%">
-                            <!-- <a href="javascript:;"> -->
                             <img src="../img/moversfax/07.png" class="img-responsive first-item" style="display: block; cursor: default;" alt="">
-                            <!-- <img src="../img/moversfax/07-g.png" class="color-img img-responsive" style="display: none; border: solid;" alt=""> -->
-                            <!-- </a> -->
                         </span>
                         <span class="client-item " style="width: 25%">
-                            <!-- <a href="javascript:;"> -->
                             <img src="../img/moversfax/09.png" class="img-responsive first-item" style="display: block; cursor: default;" alt="">
-                            <!-- <img src="../img/moversfax/09-g.png" class="color-img img-responsive" style="display: none; border: solid;" alt=""> -->
-                            <!-- </a> -->
                         </span>
                     </div>
 
                     <div class="col-md-12 m-b15">
                         <span class="client-item " style="width: 38%">
-                            <!-- <a href="javascript:;"> -->
                             <img src="../img/moversfax/11.png" class="img-responsive first-item" style="display: block; cursor: default;" alt="">
-                            <!-- <img src="../img/moversfax/11-g.png" class="color-img img-responsive" style="display: none; border: solid;" alt=""> -->
-                            <!-- </a> -->
                         </span>
-                            <span class="client-item" style="width: 11%">
-                            <!-- <a href="javascript:;"> -->
+                        <span class="client-item" style="width: 11%">
                             <img src="../img/moversfax/12.png" class="img-responsive first-item" style="display: block; cursor: default;" alt="">
-                            <!-- <img src="../img/moversfax/12-g.png" class="color-img img-responsive" style="display: none; border: solid;" alt=""> -->
-                            <!-- </a> -->
                         </span>
                     </div>
+
                 </div>
                 
+                <!-- -------- -->
                 
             </div>
         </div>
---
+++ home/new.php
@@ -0,0 +1,52 @@
+<div>
+    <div class="col-md-12 m-b15">
+        <span class="client-item " style="width: 23%">
+            <!-- <a href="javascript:;"> -->
+            <img src="../img/moversfax/01.png" class="img-responsive first-item" style="display: block; cursor: default;" alt="">
+            <!-- <img src="../img/moversfax/01-g.png" class="color-img img-responsive" style="display: none;" alt=""> -->
+            <!-- </a> -->
+        </span>
+        <span class="client-item" style="width: 13%">
+            <!-- <a href="javascript:;"> -->
+            <img src="../img/moversfax/02.png" class="img-responsive first-item" style="display: block; cursor: default;" alt="">
+            <!-- <img src="../img/moversfax/02-g.png" class="color-img img-responsive" style="display: none; border: solid;" alt=""> -->
+            <!-- </a> -->
+        </span>
+        <span class="client-item" style="width: 27%">
+            <!-- <a href="javascript:;"> -->
+            <img src="../img/moversfax/03.png" class="img-responsive first-item" style="display: block; cursor: default;" alt="">
+            <!-- <img src="../img/moversfax/03-g.png" class="color-img img-responsive" style="display: block; border: solid;" alt=""> -->
+            <!-- </a> -->
+        </span>
+    </div>
+
+    <div class="col-md-12 m-b15">
+        <span class="client-item" style="width: 24%">
+            <!-- <a href="javascript:;"> -->
+            <img src="../img/moversfax/07.png" class="img-responsive first-item" style="display: block; cursor: default;" alt="">
+            <!-- <img src="../img/moversfax/07-g.png" class="color-img img-responsive" style="display: none; border: solid;" alt=""> -->
+            <!-- </a> -->
+        </span>
+        <span class="client-item " style="width: 25%">
+            <!-- <a href="javascript:;"> -->
+            <img src="../img/moversfax/09.png" class="img-responsive first-item" style="display: block; cursor: default;" alt="">
+            <!-- <img src="../img/moversfax/09-g.png" class="color-img img-responsive" style="display: none; border: solid;" alt=""> -->
+            <!-- </a> -->
+        </span>
+    </div>
+
+    <div class="col-md-12 m-b15">
+        <span class="client-item " style="width: 38%">
+            <!-- <a href="javascript:;"> -->
+            <img src="../img/moversfax/11.png" class="img-responsive first-item" style="display: block; cursor: default;" alt="">
+            <!-- <img src="../img/moversfax/11-g.png" class="color-img img-responsive" style="display: none; border: solid;" alt=""> -->
+            <!-- </a> -->
+        </span>
+            <span class="client-item" style="width: 11%">
+            <!-- <a href="javascript:;"> -->
+            <img src="../img/moversfax/12.png" class="img-responsive first-item" style="display: block; cursor: default;" alt="">
+            <!-- <img src="../img/moversfax/12-g.png" class="color-img img-responsive" style="display: none; border: solid;" alt=""> -->
+            <!-- </a> -->
+        </span>
+    </div>
+</div>
