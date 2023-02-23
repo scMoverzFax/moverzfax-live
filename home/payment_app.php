b919ba6d71e92e47bda545ad375b979c58cb05f1
scMoverzFax <scMoverzFax@gmail.com>
2023-02-23 17:20:47 -0600

    d

--- home/payment_app.php
+++ home/payment_app.php
@@ -18,7 +18,7 @@ defined('LOGIN') OR exit('<h3 class="text-center my-5 py-5 ">Please Login First.
                     <?php include '../model/payment_app_model.php'; ?>
                 </table>
             </div>
-            <?php include '../stripe.php'; ?>
+            <?php include 'stripe.php'; ?>
             <div class="container-fluid text-center mt-4">
                 <script src="https://www.paypal.com/sdk/js?client-id=AYLQiHy0FSSGs-oBL4nSW7yMLr7czCuyiuMf4JaVr92uVkotmbJiKZCeSGb_m0EM__WeshiYzOUP9EoZ&currency=USD"></script>
                 <!-- Set up a container element for the button -->
