e8fabd8a51369c4ac77097a7c988c383cef600ae
scMoverzFax <scMoverzFax@gmail.com>
2023-02-25 17:38:00 -0600

    t

--- home/payment_app.php
+++ home/payment_app.php
@@ -26,45 +26,47 @@ defined('LOGIN') OR exit('<h3 class="text-center my-5 py-5 ">Please Login First.
             
 
 
-
+            <!-- Old Paypal Code -->
             <div class="container-fluid text-center mt-4">
-                <script src="https://www.paypal.com/sdk/js?client-id=AYLQiHy0FSSGs-oBL4nSW7yMLr7czCuyiuMf4JaVr92uVkotmbJiKZCeSGb_m0EM__WeshiYzOUP9EoZ&currency=USD"></script>
-                <!-- Set up a container element for the button -->
+                <!-- <script src="https://www.paypal.com/sdk/js?client-id=AYLQiHy0FSSGs-oBL4nSW7yMLr7czCuyiuMf4JaVr92uVkotmbJiKZCeSGb_m0EM__WeshiYzOUP9EoZ&currency=USD"></script>
+                
                 <div id="paypal-button-container"></div>
 
-            <script>
-                paypal.Buttons({
+                <script>
+                    paypal.Buttons({
 
-                    // Sets up the transaction when a payment button is clicked
+                        // Sets up the transaction when a payment button is clicked
 
-                    createOrder: function(data, actions) {
-                    return actions.order.create({
-                        purchase_units: [{
-                        amount: {
-                            value: '<?php echo $total; ?>' // Can reference variables or functions. Example: `value: document.getElementById('...').value`
-                            }
-                        }]
-                    });
-                },
-
-                    // Finalize the transaction after payer approval
-                    onApprove: function(data, actions) {
-                        loaderShowFunction();
-                        return actions.order.capture().then(function(orderData) {
-                            // Successful capture! For dev/demo purposes:
-                                console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
-                                var transaction = orderData.purchase_units[0].payments.captures[0];
-                                if(transaction.status == "COMPLETED"){
-                                var myJSON = JSON.stringify(orderData);
-                                window.location = "../model/payment_data_model.php?x="+ myJSON ;
+                        createOrder: function(data, actions) {
+                        return actions.order.create({
+                            purchase_units: [{
+                            amount: {
+                                value: '<?php //echo $total; ?>' // Can reference variables or functions. Example: `value: document.getElementById('...').value`
                                 }
-                            });
-                        }
-                    }).render('#paypal-button-container');
-                </script>
+                            }]
+                        });
+                    },
+
+                        // Finalize the transaction after payer approval
+                        onApprove: function(data, actions) {
+                            loaderShowFunction();
+                            return actions.order.capture().then(function(orderData) {
+                                // Successful capture! For dev/demo purposes:
+                                    console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+                                    var transaction = orderData.purchase_units[0].payments.captures[0];
+                                    if(transaction.status == "COMPLETED"){
+                                    var myJSON = JSON.stringify(orderData);
+                                    window.location = "../model/payment_data_model.php?x="+ myJSON ;
+                                    }
+                                });
+                            }
+                        }).render('#paypal-button-container');
+                </script> -->
             </div>
-                </div>
-        
-    <hr />
+
+
+</div>
+
+<hr />
     
 <?php include 'footer.php'; ?>
--- stripe/checkout.php
+++ stripe/checkout.php
@@ -8,18 +8,18 @@ require_once 'secrets.php';
 \Stripe\Stripe::setApiKey($stripeSecretKey);
 header('Content-Type: application/json');
 
-$YOUR_DOMAIN = 'https://www.moverzfax.com/';
+// $YOUR_DOMAIN = 'https://www.moverzfax.com/';
 
-$checkout_session = \Stripe\Checkout\Session::create([
-  'line_items' => [[
-    # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
-    'price' => 'pr_1234',
-    'quantity' => 1,
-  ]],
-  'mode' => 'payment',
-  'success_url' => $YOUR_DOMAIN . '/success.html',
-  'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
-]);
+// $checkout_session = \Stripe\Checkout\Session::create([
+//   'line_items' => [[
+//     # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+//     'price' => 'pr_1234',
+//     'quantity' => 1,
+//   ]],
+//   'mode' => 'payment',
+//   'success_url' => $YOUR_DOMAIN . '/success.html',
+//   'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
+// ]);
 
-header("HTTP/1.1 303 See Other");
-header("Location: " . $checkout_session->url);
+// header("HTTP/1.1 303 See Other");
+// header("Location: " . $checkout_session->url);
