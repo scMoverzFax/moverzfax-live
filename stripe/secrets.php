c1d40919ca4b72fd525ab231ba06743a04806dba
scMoverzFax <scMoverzFax@gmail.com>
2023-02-25 18:13:17 -0600

    g

--- stripe/secrets.php
+++ stripe/secrets.php
@@ -6,12 +6,3 @@
 // Don’t submit any personally identifiable information in requests made with this key.
 // Sign in to see your own test API key embedded in code samples.
 $stripeSecretKey = 'test';
-
-
-//////////
-
-// Set your secret key. Remember to switch to your live secret key in production.
-// See your keys here: https://dashboard.stripe.com/apikeys
-// $stripe = new \Stripe\StripeClient('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');
-
-// $stripe->products->create(['name' => 'Report']);
