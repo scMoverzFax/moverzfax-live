fed5890a88ba98351650346ec5d85d60a2841db6
scMoverzFax <scMoverzFax@gmail.com>
2023-02-25 18:06:37 -0600

    ignoring secrets.php

--- stripe/checkout.php
+++ stripe/checkout.php
@@ -13,7 +13,7 @@ $YOUR_DOMAIN = 'https://www.moverzfax.com/';
 $checkout_session = \Stripe\Checkout\Session::create([
   'line_items' => [[
     # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
-    'price' => 'pr_1234',
+    'price' => '{{PRICE_ID}}',
     'quantity' => 1,
   ]],
   'mode' => 'payment',
