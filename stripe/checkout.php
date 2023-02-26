7e8ee490c905891cfc6fc46225b7f03e74ee82e5
scMoverzFax <scMoverzFax@gmail.com>
2023-02-26 11:43:36 -0600

    v

--- stripe/checkout.php
+++ stripe/checkout.php
@@ -8,25 +8,47 @@ require_once 'secrets.php'; //secrets.php stored in cpanel file manager inside s
 \Stripe\Stripe::setApiKey($stripeSecretKey);
 header('Content-Type: application/json');
 
-$YOUR_DOMAIN = 'https://www.moverzfax.com/stripe';
 
 session_start();
 $numberOfReports = $_SESSION['numberOfReports'];
 $usdotArray = $_SESSION['usdotArray'];
 
+
 $checkout_session = \Stripe\Checkout\Session::create([
   'line_items' => [[
-    # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
-    'price' => 'price_1MfYV9EekUL6ontJJ2cyri9W',
-    'quantity' => $numberOfReports,
+    'price_data' => [
+      'currency' => 'usd',
+      'unit_amount' => 2000,
+      'product_data' => [
+        'name' => 'test',
+      ],
+    ],
+    'quantity' => 1,
+  ],
+  [
+    'price_data' => [
+      'currency' => 'usd',
+      'unit_amount' => 1000,
+      'product_data' => [
+        'name' => 'test1',
+      ],
+    ],
+    'quantity' => 1,
   ]],
   'mode' => 'payment',
-  'success_url' => $YOUR_DOMAIN . '/success.html',
-  'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
-  "metadata" => [
-    "product_name" => "My Variable Product"
-],
+  'success_url' => 'https://www.moverzfax.com/stripe/success.html',
+  'cancel_url' => 'https://www.moverzfax.com/stripe/cancel.html',
 ]);
+
+// $checkout_session = \Stripe\Checkout\Session::create([
+//   'line_items' => [[
+//     'price' => 'price_1MfYV9EekUL6ontJJ2cyri9W',
+//     'quantity' => $numberOfReports,
+//   ]],
+//   'mode' => 'payment',
+//   'success_url' => 'https://www.moverzfax.com/stripe/success.html',
+//   'cancel_url' => 'https://www.moverzfax.com/stripe/cancel.html',
+// ]);
 
 header("HTTP/1.1 303 See Other");
 header("Location: " . $checkout_session->url);
