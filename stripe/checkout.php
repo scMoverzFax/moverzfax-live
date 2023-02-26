2bc989a1552e94817d56ed621bf1a784a9f36695
scMoverzFax <scMoverzFax@gmail.com>
2023-02-26 14:33:18 -0600

    ds

--- stripe/checkout.php
+++ stripe/checkout.php
@@ -14,6 +14,7 @@ $numberOfReports = $_SESSION['numberOfReports'];
 // $usdotArray = $_SESSION['usdotArray'];
 $moverNameArray = $_SESSION['moverNameArray'];
 
+print_r($moverNameArray);
 
 
 // $products = ['product1', 'product2', 'product3', 'product4', 'product5']; // an example array of product names
