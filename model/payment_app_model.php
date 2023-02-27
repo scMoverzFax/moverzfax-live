a164af6ec5c24e27c93cabdbd6640bd73d6c700d
scMoverzFax <scMoverzFax@gmail.com>
2023-02-27 13:18:46 -0600

    s

--- model/payment_app_model.php
+++ model/payment_app_model.php
@@ -14,7 +14,7 @@
     if(mysqli_num_rows($result) > 0){ 
         while($res = mysqli_fetch_array($result)){
             // array_push($usdotArray, $res['usdot']);
-            array_push($moverNameArray, $res['company_name'] . ": USDOT #" . $res['usdot']);
+            array_push($moverNameArray, "USDOT #" . $res['usdot'] . ": " . $res['company_name']);
             $numberOfReports++;
     ?>
         <tr>
