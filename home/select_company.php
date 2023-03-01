45ab7191d9a171c6098fce5975918fa3602d795b
scMoverzFax <scMoverzFax@gmail.com>
2023-03-01 15:02:10 -0600

    ds

--- home/select_company.php
+++ home/select_company.php
@@ -230,7 +230,7 @@ include 'myheader.php';
                 <br>
 
                 <h2 class="text-center mb-5" style="font-weight: 500">Please Login First...</h2>
-                <p>
+                <p class="text-center">
                     <a href="signin.php">Login </a> <span>|</span><a href="register.php"> Register</a>
                 </p>
                 
