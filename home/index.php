41f756d1a73270b02ddb9516f78a78666ce8cc6c
scMoverzFax <scMoverzFax@gmail.com>
2023-03-01 18:00:22 -0600

    c

--- home/index.php
+++ home/index.php
@@ -473,7 +473,7 @@
                 
                 <div class="wo-image mt-20">
                     <!-- <img src="https://www.moversfax.com/frontend/img/wo.png" width="100%">  -->
-                    <img src="../img/moversfax/woman.avif" width="100%">
+                    <img src="../img/moversfax/woman.png" width="100%">
                 </div>
                 <div class="wo-services">
                    
