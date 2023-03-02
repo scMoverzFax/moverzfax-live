1bdfc39eb463976d28c6320de23f7bb60104a6fc
scMoverzFax <scMoverzFax@gmail.com>
2023-03-01 18:16:56 -0600

    v

--- home/index.php
+++ home/index.php
@@ -627,7 +627,7 @@
                         <div class="tab-pane row fade active in" id="tab-1">
                             <div class="col-md-3 col-sm-3">
                                 <!-- <a href="https://www.moversfax.com/frontend/temp/photos/img7.jpg" class="fancybox-button" title="Image Title" data-rel="fancybox-button"> -->
-                                    <img style="height: 100px;" src="https://www.moversfax.com/frontend/pages/img/photos/img7.jpg" alt="">
+                                    <img style="height: 100px; width: auto;" src="https://www.moversfax.com/frontend/pages/img/photos/img7.jpg" alt="">
                                 <!-- </a> -->
                             </div>
                             <div class="col-md-9 col-sm-9">
