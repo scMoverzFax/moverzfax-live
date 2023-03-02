4857f026f0c04ff46e45286e594f3e3388eecfd2
scMoverzFax <scMoverzFax@gmail.com>
2023-03-02 11:38:35 -0600

    c

--- home/index.php
+++ home/index.php
@@ -2,7 +2,7 @@
 
 <head>
     <title>MoverZfax - Home</title>
-    <meta charset="utf-8">
+        <meta charset="utf-8">
         <meta content="width=device-width, initial-scale=1.0" name="viewport">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <!-- CSRF Token -->
