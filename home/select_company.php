dd232fc025a2e9e9cd3392748e157a0b405b33c1
scMoverzFax <scMoverzFax@gmail.com>
2023-04-29 11:34:08 -0500

    see

--- home/select_company.php
+++ home/select_company.php
@@ -169,7 +169,7 @@ if (!isset($_SESSION["id"])) {
                         </p>
                     <?php } ?>
 
-                    <form action="../model/select_operation.php" name="usdot" class="m-3" method="post" style="margin-top:50px; border:black 1px solid;">
+                    <!-- <form action="../model/select_operation.php" name="usdot" class="m-3" method="post" style="margin-top:50px; border:black 1px solid;">
                         <div class="row">
                             <div class="d-flex col-md-12 form-group">
                                 <div class="col-md-4 search">
@@ -185,27 +185,67 @@ if (!isset($_SESSION["id"])) {
                                         <span class="visually-hidden">Loading...</span>
                                     </div>
                                     <div class="d-flex">
-                                        <span class="me-2" id="cart_label1"><?php echo $usdot; ?></span>
+                                        <span class="me-2" id="cart_label1"><?php //echo $usdot; ?></span>
                                         <span for="" id="cart_label2">
                                             <?php 
-                                                if ($status == "as") {
-                                                    echo "was added successfully.";
-                                                } elseif ($status == "nr") {
-                                                    echo "is not registered with MoverzFax.";
-                                                } elseif ($status == "ae") {
-                                                    echo "already exist.";
-                                                } elseif ($status == "rf") {
-                                                    echo "Request Failed.";
-                                                } else {
-                                                    echo " "; //edge case
-                                                }
+                                                // if ($status == "as") {
+                                                //     echo "was added successfully.";
+                                                // } elseif ($status == "nr") {
+                                                //     echo "is not registered with MoverzFax.";
+                                                // } elseif ($status == "ae") {
+                                                //     echo "already exist.";
+                                                // } elseif ($status == "rf") {
+                                                //     echo "Request Failed.";
+                                                // } else {
+                                                //     echo " "; //edge case
+                                                // }
                                             ?>
                                         </span>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </form>
+                    </form> -->
+                    <form action="../model/select_operation.php" name="usdot" class="m-3" method="post" style="margin-top:50px; border:black 1px solid;">
+    <div class="row align-items-center">
+        <div class="col-md-4">
+            <label for="usdot-input" class="form-label">Add A Mover:</label>
+        </div>
+        <div class="col-md-7">
+            <input type="text" name="usdot" id="usdot-input" class="form-control form-control-sm" placeholder="Enter #USDOT Number" required>
+            <input name="function" type="hidden" value="search">
+        </div>
+        <div class="col-md-1 pl-0">
+            <input type="submit" class="btn btn-success btn-sm w-100" style="height: calc(1.5em + .75rem + 2px);" value="Go">
+        </div>
+    </div>
+    <div class="row mt-2">
+        <div class="col-md-12" style="height:40px;">
+            <div class="spinner-border text-success" id="cart_spinner" role="status" style="display:none;">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+            <div class="d-flex">
+                <span class="me-2" id="cart_label1"><?php echo $usdot; ?></span>
+                <span for="" id="cart_label2">
+                    <?php 
+                        if ($status == "as") {
+                            echo "was added successfully.";
+                        } elseif ($status == "nr") {
+                            echo "is not registered with MoverzFax.";
+                        } elseif ($status == "ae") {
+                            echo "already exist.";
+                        } elseif ($status == "rf") {
+                            echo "Request Failed.";
+                        } else {
+                            echo " "; //edge case
+                        }
+                    ?>
+                </span>
+            </div>
+        </div>
+    </div>
+</form>
+
 
                     <span Class="text-center text-danger"></span>
                     <div class="text-center">
