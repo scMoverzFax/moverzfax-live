019eb13d7469942840e8b28e7807d74c3aa8df82
scMoverzFax <scMoverzFax@gmail.com>
2023-04-29 13:22:20 -0500

    we

--- home/myheader.php
+++ home/myheader.php
@@ -41,7 +41,7 @@ if (isset($_SESSION['catagory']) && $_SESSION['catagory'] == "customer") {
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
     <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
     <!-- Popper JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
