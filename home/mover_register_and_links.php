2a214720d03ae21c471ad3443800afa7acfef1cb
scMoverzFax <scMoverzFax@gmail.com>
2023-05-17 12:26:19 -0500

    cee

--- home/mover_register_and_links.php
+++ home/mover_register_and_links.php
@@ -436,7 +436,7 @@ if(isset($_REQUEST["usdot-check"])){
 								<tbody>
 									<tr>
 										<td></td>
-										<td><input type="checkbox" name="" required> I agree to the <a href="terms_of_use.pdf" target="_blank">terms of use</a></td>
+										<td><input type="checkbox" name="agreement" required> I agree to the <a href="terms_of_use.pdf" target="_blank">terms of use</a></td>
 									</tr>
 								</tbody>
 							</table>
