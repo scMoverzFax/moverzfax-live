a5a4f12f64ef8a1e810d1724eac9d97d3e7876c1
scMoverzFax <scMoverzFax@gmail.com>
2023-05-17 12:58:41 -0500

    h

--- home/mover_register_and_links.php
+++ home/mover_register_and_links.php
@@ -90,6 +90,15 @@
 		border-color: #bd2130;
 	}
 
+	.section-1, .section-2 {
+		position: relative;
+		z-index: 1;
+	}
+
+	.hidden {
+		z-index: -1;
+	}
+
 	/* Desktop-mobile approach --------------------------------------------------------------*/
 
 	/* smaller than Desktop HD */
@@ -464,30 +473,51 @@ if(isset($_REQUEST["usdot-check"])){
 <script src="../js/csc_sort_1.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script>
-	$(document).ready(function() {
-        $('#continue').click(function() {
-            // Optionally, validate form input here.
-			var form = $('#mv-reg-form')[0];
-            if (form.checkValidity() === false) {
-                event.preventDefault();
-                event.stopPropagation();
-				form.reportValidity();
-				// Force a focus on the first invalid element.
-            	$(form).find(':invalid').first().focus();
-            } else {
+	// $(document).ready(function() {
+    //     $('#continue').click(function() {
+    //         // Optionally, validate form input here.
+	// 		var form = $('#mv-reg-form')[0];
+    //         if (form.checkValidity() === false) {
+    //             event.preventDefault();
+    //             event.stopPropagation();
+	// 			form.reportValidity();
+	// 			// Force a focus on the first invalid element.
+    //         	$(form).find(':invalid').first().focus();
+    //         } else {
 
-				// Hide the first section and show the second.
-				$(".section-1").hide();
-				$(".section-2").show();
+	// 			// Hide the first section and show the second.
+	// 			$(".section-1").hide();
+	// 			$(".section-2").show();
+	// 			window.scrollTo(0, 0);
+	// 		}
+    //     });
+	// 	$("#back-button").click(function () {
+	// 		$(".section-2").hide();
+	// 		$(".section-1").show();
+    //     	window.scrollTo(0, 0);
+	// 	});
+    // });
+	$(document).ready(function() {
+		$('#continue').click(function(event) {
+			var form = $('#mv-reg-form')[0];
+			if (form.checkValidity() === false) {
+				event.preventDefault();
+				event.stopPropagation();
+				form.reportValidity();
+			} else {
+				$(".section-1").addClass('hidden');
+				$(".section-2").removeClass('hidden');
 				window.scrollTo(0, 0);
 			}
-        });
-		$("#back-button").click(function () {
-			$(".section-2").hide();
-			$(".section-1").show();
-        	window.scrollTo(0, 0);
 		});
-    });
+
+		$("#back-button").click(function () {
+			$(".section-2").addClass('hidden');
+			$(".section-1").removeClass('hidden');
+			window.scrollTo(0, 0);
+		});
+	});
+
 
 	function resetToTop() {
 		// Get the current URL
