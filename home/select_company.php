beb513829c88c4b861bc79c54873adcb13cb1399
scMoverzFax <scMoverzFax@gmail.com>
2023-08-23 10:21:03 -0500

    c

--- home/select_company.php
+++ home/select_company.php
@@ -16,19 +16,13 @@ include 'myheader.php';
         position: relative;
         padding: 50px;
         max-width: 1440px;
-
-    }
-
-    .b-search{
-        position: relative;
     }
 
     .b-search {
         position: relative;
     }
 
-    .b-search input,
-    button {
+    .b-search input, button {
         font-family: 'Poppins', sans-serif;
         letter-spacing: .8px;
     }
@@ -438,32 +432,31 @@ include 'myheader.php';
                     </p>
                 <?php } ?>
             </div>
-                
+
             <!-- Search USDOT and Result Message -->
-            <div class="b-search"> 
-            <form action="../model/select_operation.php" name="usdot" method="post" class="border border-secondary">
+            <div class="b-search border border-secondary">
+                <form action="../model/select_operation.php" name="usdot" method="post">
 
-                    <!-- <input type="hidden" name="_token" value="SD49uC9YMu0Jcm972BpNLVnuT4gcNjI0pZ3HQBk4"> -->
-                    <div class="inner-form">
-                        <div class="input-field first-wrap">
-                            <div class="svg-wrapper">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                                    <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
-                                </svg>
+                        <!-- <input type="hidden" name="_token" value="SD49uC9YMu0Jcm972BpNLVnuT4gcNjI0pZ3HQBk4"> -->
+                        <div class="inner-form">
+                            <div class="input-field first-wrap">
+                                <div class="svg-wrapper">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                        <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+                                    </svg>
+                                </div>
+                                <input id="search" name="usdot" type="text" placeholder="Search USDOT Registration..." required>
+                                <input name="function" type="hidden" value="search">
                             </div>
-                            <input id="search" name="usdot" type="text" placeholder="Search USDOT Registration..." required>
-                            <input name="function" type="hidden" value="search">
+                            <div class="input-field second-wrap">
+                                <button class="btn-search" type="submit" required>USDOT #</button>
+                            </div>
                         </div>
-                        <div class="input-field second-wrap">
-                            <button class="btn-search" type="submit" required>USDOT #</button>
-                        </div>
-                    </div>
 
-                <!-- </div> -->
-            </form>
+                </form>
             </div>
 
-            <!-- Show Result of Search -->
+            <!-- Result of Search Message -->
             <div class="contain-result">
 
                 <div class="spinner-border text-success" id="cart_spinner" role="status" style="display:none;">
