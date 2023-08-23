cd5101d0801c6c38d310d4dba15f8934f4b73f95
scMoverzFax <scMoverzFax@gmail.com>
2023-08-23 14:02:23 -0500

    t

--- home/mover_register_and_links.php
+++ home/mover_register_and_links.php
@@ -294,7 +294,7 @@ if(isset($_REQUEST["usdot-check"])){
 						<!-- <h5 class="text-danger text-center"><?= $msg ?><?= $cmsg ?></h5> -->
 
 						<form class="mt-4 border border-primary" method="get" action="mover_register_and_links.php">
-							<div class=" mx-auto d-flex align-items-center mb-3">
+							<div class="border border-danger mx-auto d-flex align-items-center mb-3">
 								<input type="text" class="form-control mr-3" id="usdot-check" name="usdot-check" value="" placeholder="Enter Your USDOT" required>
 								<button type="submit" class="btn button-mf me-5">Validate</button>
 							</div>
