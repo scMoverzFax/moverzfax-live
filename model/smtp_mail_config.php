325b4e6966f9bf62a33005d195443f80d147fb8d
scMoverzFax <scMoverzFax@gmail.com>
2023-10-28 14:17:09 -0500

    Updated to google SMTP

--- model/smtp_mail_config.php
+++ model/smtp_mail_config.php
@@ -4,18 +4,28 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 require '../vendor/autoload.php';
 
+// This function no longer uses MailRelay, but the function name will stay the same
 function sendMailViaMailrelay($to, $subject, $message, $isHTML = true) {
     $mail = new PHPMailer(true);
 
     try {
         // Set up your SMTP settings here
         $mail->isSMTP();
-        $mail->Host       = 'smtp1.s.ipzmarketing.com';  // Replace with your Mailrelay SMTP details
+        // Old MailRelay credentials
+        // $mail->Host       = 'smtp1.s.ipzmarketing.com';  // Replace with your Mailrelay SMTP details
+        // $mail->SMTPAuth   = true;
+        // $mail->Username   = 'xjbmllevnemd';              // Replace with your username
+        // $mail->Password   = 'JD_LL8rgILI';              // Replace with your password
+        // $mail->SMTPSecure = 'tls';                   // Or 'ssl', based on your Mailrelay configuration
+        // $mail->Port       = 587;                     // Or 465 for 'ssl'
+
+        // Switched to Google SMTP
+        $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'xjbmllevnemd';              // Replace with your username
-        $mail->Password   = 'JD_LL8rgILI';              // Replace with your password
-        $mail->SMTPSecure = 'tls';                   // Or 'ssl', based on your Mailrelay configuration
-        $mail->Port       = 587;                     // Or 465 for 'ssl'
+        $mail->Username   = 'moverzfax@gmail.com';   // Replace with your Gmail email
+        $mail->Password   = 'consumerprotect2023@#';         // Replace with your Gmail password
+        $mail->SMTPSecure = 'tls';                   // Use 'ssl' if you choose Port 465
+        $mail->Port       = 587;                     // Use 465 for 'ssl'
 
         $mail->setFrom('admin@moverzfax.com');
         $mail->addAddress($to);
